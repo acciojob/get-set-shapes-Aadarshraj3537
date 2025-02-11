ef5049a38e553adeddd9bff1fd3141de4e840326
Aadarsh Raj <aadarshraj351@gmail.com>
2025-02-11 13:13:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,16 +32,3 @@ class Square extends Rectangle {
   }
 }
 
-// Example Usage
-const rectangle = new Rectangle(5, 10);
-console.log(rectangle.width); 
-console.log(rectangle.height); 
-console.log(rectangle.getArea()); 
-
-const square = new Square(7);
-console.log(square.width); 
-console.log(square.height); 
-console.log(square.getArea()); 
-console.log(square.getPerimeter());
-
-
